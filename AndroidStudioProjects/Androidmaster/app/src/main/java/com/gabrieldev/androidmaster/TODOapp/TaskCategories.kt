b49245d3d1263ac7963a652839e2125251cf95b0
gsat0301 <109878564+gsat0301@users.noreply.github.com>
2025-06-19 17:12:26 -0400

    Delete mistake archives commit

--- AndroidStudioProjects/Androidmaster/app/src/main/java/com/gabrieldev/androidmaster/TODOapp/TaskCategories.kt
+++
@@ -1,7 +0,0 @@
-package com.gabrieldev.androidmaster.TODOapp
-
-sealed class TaskCategories(var isSelected :Boolean = true) {
-    object Personal: TaskCategories()
-    object Business: TaskCategories()
-    object Other: TaskCategories()
-}
--- AndroidStudioProjects/Androidmaster/app/src/main/java/com/gabrieldev/androidmaster/sintaxis/Nulavilidad.kt
+++
@@ -1,6 +0,0 @@
-package com.gabrieldev.androidmaster.sintaxis
-
-fun main(){
-var name:String? = null
-    println(name?.get(3) ?: "Es nulo")
-}
